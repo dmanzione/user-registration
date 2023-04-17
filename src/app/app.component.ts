055ca5a0405b9f4b029e5d325ba6b4a42834a4a3
Donato Manzione <donato.manzione@smoothstack.com>
2023-04-17 00:55:52 -0400

    c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
 import { setTheme } from 'ngx-bootstrap/utils';
-import { MatListOption } from '@angular/material/list';
-import { MatNavList } from '@angular/material/list';
+
 
 @Component({
   selector: 'app-root',
@@ -12,21 +10,14 @@ import { MatNavList } from '@angular/material/list';
 })
 export class AppComponent {
   title = 'user-registration';
-  activatedRoute: ActivatedRoute;
-  
-   
 
 
 
-   constructor(activatedRoute:ActivatedRoute) {
-      this.activatedRoute = activatedRoute; 
-     setTheme('bs5');
-   }
 
 
-   getRoute():string{
-    return this.activatedRoute.snapshot.params[0];
-   }
-      
-   
+  constructor() {
+    setTheme('bs5');
+  }
+
+
 }
